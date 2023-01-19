ce9de9811f6bfc9828a573afa564313769f4f591
liye <liye@gmail.com>
2023-01-20 02:16:37 +0800

    媒资模块开发、上传图片业务开发

---
+++ xuecheng-media/xuecheng-media-service/src/main/java/com/xuecheng/media/mapper/MediaProcessMapper.java
@@ -0,0 +1,12 @@
+package com.xuecheng.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.media.model.po.MediaProcess;
+
+/**
+ * <p>
+ *  Mapper接口
+ * </p>
+ */
+public interface MediaProcessMapper extends BaseMapper<MediaProcess> {
+}
---
+++ xuecheng-media/xuecheng-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -0,0 +1,37 @@
+package com.xuecheng.media.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
+import com.xuecheng.media.model.po.MediaFiles;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * 媒资文件管理业务类
+ */
+public interface MediaFileService {
+
+    /**
+     * @param pageParams          分页参数
+     * @param queryMediaParamsDto 查询条件
+     * @description 媒资文件查询方法
+     */
+    PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+
+    /**
+     * @param companyId           机构id
+     * @param uploadFileParamsDto 文件信息
+     * @param bytes               文件字节数组
+     * @param folder              桶下边的子目录
+     * @param objectName          对象名称
+     * @description 上传文件的通用接口
+     */
+    UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, byte[] bytes, String folder, String objectName);
+
+    //上传到数据库
+    MediaFiles addMediaFilesToDb(Long companyId, String fileId, UploadFileParamsDto uploadFileParamsDto, String bucket, String objectName);
+}
